7143ff3fcf3eeb674b516785809c18bae40a4158
Mark Kooijman <kooi.barlaeus@gmail.com>
2022-07-20 15:49:38 +0200

    Added debug option for chowing timings

--- src/game.py
+++ src/game.py
@@ -1,8 +1,18 @@
 import arcade
+from dinogame import DEBUG, TIMINGS_COUNTER
 from dinogame.gamescreen import GameScreen
 
 # Main code entry point
 if __name__ == "__main__":
     game = GameScreen()
     game.setup()
+
+
+    # DEBUG: Timings
+    if DEBUG:
+        arcade.enable_timings()
     arcade.run()
+    
+    if DEBUG:
+        print('fps:', arcade.get_fps(1200))
+        arcade.print_timings()
